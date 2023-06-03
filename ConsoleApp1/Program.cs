75b271b8f15d686598327c766357099dd1711bae
shhyc1001backup <126031288+ZizonLonlyWolf@users.noreply.github.com>
2023-06-03 17:02:32 +0900

    [EDIT] Hell World->Well Neo로 변경
    메인문에서 일부 코드 수정

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell World");
+            Console.WriteLine("Well Neo");
             Console.WriteLine("Well Well, Mr 엔더슨");
         }
     }
